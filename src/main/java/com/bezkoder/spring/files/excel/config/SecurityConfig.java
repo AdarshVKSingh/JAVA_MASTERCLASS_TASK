63da343256b91e1b1273e28144bb5477c49a864f
Adarsh Vikram Singh <103936096+AdarshVKSingh@users.noreply.github.com>
2024-05-05 20:56:53 +0530

    Update SecurityConfig.java

--- src/main/java/com/bezkoder/spring/files/excel/config/SecurityConfig.java
+++ src/main/java/com/bezkoder/spring/files/excel/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.bezkoder.spring.files.excel.config.SecurityConfig;
+package com.bezkoder.spring.files.excel.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
